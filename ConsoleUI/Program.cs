86605176734c06562b624640b8640fda050665a5
burakhatipogluu <burakhatipogluu@hotmail.com>
2021-03-11 16:40:22 +0300

    20210311

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -12,9 +12,21 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {                                                                           //EfProductDal
+            //ProductTest();
+
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());//InMemoryProductDal
 
-            foreach (var product in productManager.GetByUnitPrice(50,100))
+            foreach (var product in productManager.GetByUnitPrice(50, 100))
             {
                 Console.WriteLine(product.ProductName);
             }
