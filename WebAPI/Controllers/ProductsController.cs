643d6cb16614e5d15c606a651f71b7bf3194be27
burakhatipogluu <77885766+burakhatipogluu@users.noreply.github.com>
2021-03-21 22:38:05 +0300

    20210321

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -21,11 +21,33 @@ namespace WebAPI.Controllers
         {
             _productService = productService;
         }
-
-        public IActionResult Get()
+        [HttpGet("getall")]
+        public IActionResult GetAll()
         {
             
             var result = _productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
             if (result.Success)
             {
                 return Ok(result);
